c3a0b906df7f32e5437ae55e26ae0a31af730aa3
orhanucr <ucar.orh@gmail.com>
2023-05-24 15:10:14 +0300

    Update
    the functions of the pages have been created

--- Tabs/app/src/main/java/com/example/tabs/MainActivity.kt
+++ Tabs/app/src/main/java/com/example/tabs/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.tabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tabs.ui.theme.TabsTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +19,53 @@ class MainActivity : ComponentActivity() {
         setContent {
 
         }
+    }
+}
+
+@Composable
+fun HomePage() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        ) {
+            Text(
+                text = "Home Screen",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontSize = 30.sp
+            )
+    }
+}
+
+@Composable
+fun ProfilePage() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Profile Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 30.sp
+        )
+    }
+}
+
+@Composable
+fun SettingsPage() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Settings Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 30.sp
+        )
     }
 }
